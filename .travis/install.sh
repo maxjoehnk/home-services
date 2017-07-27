0b23055aa0a9a708219629e29d99616866ead34b
Max Jöhnk <maxjoehnk@gmail.com>
2017-07-27 16:44:47 +0200

    [travis] add status messages to scripts

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 root=$(pwd)
 for service in services/*/ ; do
+    echo "Installing Dependencies of $(basename $service)..."
     cd "$root/$service"
     npm i --ignore-scripts
 done
--- .travis/lint.sh
+++ .travis/lint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 root=$(pwd)
 for service in services/*/ ; do
+    echo "Linting $(basename $service)..."
     cd "$root/$service"
     npm run lint
 done
