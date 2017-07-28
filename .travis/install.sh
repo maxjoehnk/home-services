25fff953ce8a9b4b4a4f9c0eef1df3150de82e29
Max Jöhnk <maxjoehnk@gmail.com>
2017-07-28 23:46:42 +0200

    [travis] update scripts to handle shared libraries

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env sh
 root=$(pwd)
+
+for library in shared/*/ ; do
+    echo "Installing dependencies of library $(basename $library)..."
+    cd "$root/$library"
+    npm i --ignore-scripts
+done
+
+cd $root
+
 for service in services/*/ ; do
-    echo "Installing Dependencies of $(basename $service)..."
+    echo "Installing Dependencies of service $(basename $service)..."
     cd "$root/$service"
     npm i --ignore-scripts
 done
--- .travis/lint.sh
+++ .travis/lint.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 root=$(pwd)
+for library in shared/*/ ; do
+    echo "Linting library $(basename $library)..."
+    cd "$root/$library"
+    npm run lint
+done
+
+cd $root
+
 for service in services/*/ ; do
-    echo "Linting $(basename $service)..."
+    echo "Linting service $(basename $service)..."
     cd "$root/$service"
     npm run lint
 done
