6cc057db384abd02775742a92072564c6d314ae9
Max Jöhnk <maxjoehnk@gmail.com>
2017-08-01 22:50:09 +0200

    wip

---
+++ services/generic-proxy/src/index.js
@@ -0,0 +1,73 @@
+// @flow
+
+const fs = require('fs');
+const { resolve } = require('path');
+// $FlowFixMe
+const { promisify } = require('util');
+const { safeLoad } = require('js-yaml');
+const logger = require('./logger');
+
+const readFile = promisify(fs.readFile);
+
+/*::
+type Route = {
+    url: stirng
+};
+
+type Routes = {
+    [string]: Route
+};
+
+type Config = {
+    port: number,
+    routes: Routes
+};
+
+type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal';
+
+type Options = {
+    config: string,
+    logLevel: LogLevel
+};
+*/
+
+async function start(args /* :any */) {
+    try {
+        const options = defaultOptions(args);
+        logger.level(options.logLevel);
+        const config = await loadConfig(options.config);
+        setupRoutes(config.routes);
+    }catch (err) {
+        logger.fatal(err);
+    }
+}
+
+function setupRoutes(routes /* :Routes */) /* :void */ {
+
+}
+
+function defaultOptions(options /* :Options */) /* :Options*/ {
+    const defaults = {
+        config: resolve(__dirname, '../config.yml'),
+        logLevel: 'warn'
+    };
+    return Object.assign({}, defaults, options);
+}
+
+function defaultConfig(config /* :Config */) /* :Config */ {
+    const defaults = {
+        port: 8080,
+        routes: {}
+    };
+    return Object.assign({}, defaults, config);
+}
+
+async function loadConfig(path /* :string */) /* :Promise<Config> */ {
+    const file = await readFile(path, 'utf8');
+    const config = safeLoad(file); //TODO: Validate result
+    return defaultConfig(config);
+}
+
+module.exports = {
+    start
+};
---
+++ services/generic-proxy/src/logger.js
@@ -0,0 +1,13 @@
+// @flow
+
+const { createLogger } = require('bunyan');
+const formatter = require('bunyan-format');
+
+const logger = createLogger({
+    name: 'proxy-service',
+    stream: formatter({
+        outputMode: 'short'
+    })
+});
+
+module.exports = logger;
