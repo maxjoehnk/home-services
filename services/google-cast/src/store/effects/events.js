7aa7d605b85ef0058d354813556747f1093d1084
Max Jöhnk <maxjoehnk@gmail.com>
2017-08-10 09:44:58 +0200

    add volume events and fix application filtering bug

--- services/google-cast/src/store/effects/events.js
+++ services/google-cast/src/store/effects/events.js
@@ -2,7 +2,10 @@ const { takeEvery, select } = require('redux-saga/effects');
 const logger = require('../../logger');
 const {
     APPLICATION_LAUNCH,
-    APPLICATION_EXIT
+    APPLICATION_EXIT,
+    VOLUME_CHANGE,
+    VOLUME_MUTE,
+    VOLUME_UNMUTE
 } = require('../actions');
 const execute = require('../../../../../shared/url-executor');
 
@@ -14,13 +17,14 @@ function emit(event) {
         }
         const name = yield select(({ names }) => names[id]);
         const emitters = yield select(({ events }) => events[name] ? events[name][event] : []);
-        if (emitters) {
+        if (emitters && emitters.length > 0) {
             logger.debug(`Emitting ${event} for ${id} (${name})`);
             const application = yield select(({ devices }) => devices[id].application);
             emitters.forEach(emitter => {
                 if (emitter.applications) {
-                    if (!emitter.applications.include(application.displayName)) {
+                    if (!emitter.applications.includes(application.displayName)) {
                         logger.debug(`Skipping Event Execution for Event ${event}`, emitter);
+                        return;
                     }
                 }
                 logger.debug(`Executing Event Handler for ${event}`, emitter);
@@ -35,6 +39,9 @@ function emit(event) {
 function* eventsSaga() {
     yield takeEvery(APPLICATION_LAUNCH, emit('launch'));
     yield takeEvery(APPLICATION_EXIT, emit('exit'));
+    yield takeEvery(VOLUME_CHANGE, emit('volume'));
+    yield takeEvery(VOLUME_MUTE, emit('mute'));
+    yield takeEvery(VOLUME_UNMUTE, emit('unmute'));
 }
 
 module.exports = eventsSaga;
